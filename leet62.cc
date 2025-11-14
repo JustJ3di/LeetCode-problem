6cbd61eae2fdc8c03f03278eb3b4a33d148a4d00
Emanuele d'Ajello <55718523+JustJ3di@users.noreply.github.com>
2025-11-14 23:35:23 +0100

    Create leet62.cc

---
+++ leet62.cc
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector < vector <int> > board;
+        board.resize(m,vector<int>(n));
+        board[m-1][n-1] = 1;
+        return backtracking(board, m, n, 0, 0);
+    }
+     int backtracking(vector <vector <int>>  &board, int m, int n, int r, int c){
+        if(r == m || c == n) return 0;
+        if(board[r][c] != 0) return board[r][c];
+        int n1 = backtracking(board, m, n, r+1, c);
+        int n2 = backtracking(board, m, n, r, c+1);
+        board[r][c] = n1 + n2;
+        return board[r][c];
+     }
+
+};
