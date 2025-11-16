ae3ecf939cbf5b9c293beb31bbc22a2ea770a6a1
Emanuele d'Ajello <55718523+JustJ3di@users.noreply.github.com>
2025-11-17 00:29:09 +0100

    Create leet2057.cc

---
+++ leet2057.cc
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int smallestEqual(vector<int>& nums) {
+        for(auto i = 0;i<nums.size();++i){
+            if(i%10==nums[i])return i;
+        }
+        return -1;
+    }
+};
