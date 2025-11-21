8b0b26d7282888f08d9f0c726fc148fbcdf7dc42
Emanuele d'Ajello <55718523+JustJ3di@users.noreply.github.com>
2025-11-21 19:04:28 +0100

    Create leet2210.cc

---
+++ leet2210.cc
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countHillValley(vector<int>& nums) {
+        int c = 0;
+        int prev = 0;
+        for(int i = 1;i<nums.size()-1;++i){
+            if(nums[i] != nums[i+1]){
+                //is a hill                                  or   is a valley
+                if(nums[i] > nums[prev] && nums[i] > nums[i+1] || nums[i] < nums[prev] && nums[i] < nums[i+1])
+                    c++;  
+            prev = i;
+            }
+        }
+        return c;
+    }
+};
